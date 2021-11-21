8163a35bd44a08a12e53fa3151bc9ecdfcae8af5
Elton Possidonio <eltonrpo@gmail.com>
2021-11-21 16:02:23 -0300

    correcao static_root

--- reforco_online/settings.py
+++ reforco_online/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# STATIC_ROOT = [APP_ROOT, 'static/']
 
 STATIC_URL = '/static/'
 
@@ -130,6 +131,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR, 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
