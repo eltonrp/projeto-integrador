76e1e67ae9fd09f28a49472a9425bfc343c1d06d
Elton Possidonio <eltonrpo@gmail.com>
2021-11-21 16:30:21 -0300

    novas configurações

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn reforco_online.wsgi
--- reforco_online/settings.py
+++ reforco_online/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,9 +134,7 @@ STATICFILES_DIRS = [
     BASE_DIR, 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
